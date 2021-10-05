dae9d219f9d85951c6535058cb980a10ec9d7bc2
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-10-05 15:23:48 +0300

    Update ChatScreen.js

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -112,7 +112,7 @@ const ChatScreen = ({ navigation, route }) => {
                     <Text style={styles.recieverText}>{data.message}</Text>
                   </View>
                 ) : (
-                  <View style={styles.sender}>
+                  <View key={id} style={styles.sender}>
                     <Avatar
                       position="absolute"
                       bottom={-15}
